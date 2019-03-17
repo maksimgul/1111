8f4b9adc2858f4aa953fcc32b7a93ea6e4169fda
maksimgul <46872288+maksimgul@users.noreply.github.com>
2019-03-17 14:00:03 +0300

    Add files via upload

---
+++ JavaApplication69/src/javaapplication69/Wild.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication69;
+
+/**
+ *
+ * @author PC
+ */
+public class Wild extends Animals {
+    private int max_tired = 100;
+    private int now_tired = 50;
+    int hunting = 15;
+
+}
